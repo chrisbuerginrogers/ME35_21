f8d6c77450eb737c2354a3595685c5b73a672d7e
chrisbuerginrogers <crogers@tufts.edu>
2021-02-04 05:58:27 -0500

    Fixed spacing issue

--- PythonIDE/PythonCodes/scratch.py
+++ PythonIDE/PythonCodes/scratch.py
@@ -1,7 +1,7 @@
 import hub,utime
 
 dial = hub.port.A.motor
-dial.mode(2) # 2 is rotations and 3 is degrees
+dial.mode(2) # if no motor is attached you will get an error
 dial.float()  # so the motor does not push back
 
 while True:
@@ -9,3 +9,5 @@ while True:
     print(clicks)
     utime.sleep(0.1)
     
+
+            
