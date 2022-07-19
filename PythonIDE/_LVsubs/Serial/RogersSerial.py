8a9461ad06af0641240472b896c1ae197adb2d8a
chrisbuerginrogers <crogers@tufts.edu>
2022-07-19 16:17:56 -0400

    fixed port on PC

--- PythonIDE/_LVsubs/Serial/RogersSerial.py
+++ PythonIDE/_LVsubs/Serial/RogersSerial.py
@@ -2,26 +2,17 @@ import serial
 import sys
 import glob
 import time
+import serial.tools.list_ports
 
 ser = None
 
 def serial_ports():
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-        return ports
-    else:
-        raise EnvironmentError('Unsupported platform')
-    result = []
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
+     result = []
+     ports = serial.tools.list_ports.comports()
+     for port, desc, hwid in sorted(ports):
+          comm =  "{}: {}".format(port, desc)
+          result.append(comm) 
+     return result
 
 def InitSerial(port, bps = 9600, to = 0):
     global ser
